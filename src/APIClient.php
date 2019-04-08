ba3a7083cdb1e9ab9236b4b6e3dfb7f34e0d247c
Artur Szymczyk <arturszymczyk1@gmail.com>
2019-04-08 10:25:05 +0200

    Initial commit

---
+++ src/APIClient.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Arrrni\FreshsalesAPI;
+
+/**
+ * Class APIClient
+ * @package Arrrni\FreshsalesAPI
+ */
+class APIClient
+{
+
+}
---
+++ tests/Unit/DummyTest.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Arrrni\FreshsalesAPI\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Class DummyTest
+ * @package Arrrni\FreshsalesAPI\Unit
+ */
+class DummyTest extends TestCase
+{
+    public function testDummy(): void
+    {
+        $this->assertTrue(true);
+    }
+}
